05a5e2979298d77c8b816b8025d5ef4f7a50a36a
Muhammad Saqib Chaudhry <young_innovator@yahoo.com>
2023-10-14 21:29:49 +0200

    SymptomController fixed + removed unused imports

--- monolithic/monolithic-layered/src/main/java/com/epam/training/microservices/monolithic/web/DrugController.java
+++ monolithic/monolithic-layered/src/main/java/com/epam/training/microservices/monolithic/web/DrugController.java
@@ -10,7 +10,6 @@ import com.epam.training.microservices.monolithic.web.crud.single.ViewSingleTemp
 import com.epam.training.microservices.monolithic.web.crud.single.form.HiddenFieldModel;
 import com.epam.training.microservices.monolithic.web.crud.single.form.TextFieldModel;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
--- monolithic/monolithic-layered/src/main/java/com/epam/training/microservices/monolithic/web/SymptomController.java
+++ monolithic/monolithic-layered/src/main/java/com/epam/training/microservices/monolithic/web/SymptomController.java
@@ -11,14 +11,13 @@ import com.epam.training.microservices.monolithic.web.crud.single.ViewSingleTemp
 import com.epam.training.microservices.monolithic.web.crud.single.form.HiddenFieldModel;
 import com.epam.training.microservices.monolithic.web.crud.single.form.TextFieldModel;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * A controller to manage symptoms. 
  */
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/symptoms")
 public class SymptomController implements CrudController<Symptom> {
