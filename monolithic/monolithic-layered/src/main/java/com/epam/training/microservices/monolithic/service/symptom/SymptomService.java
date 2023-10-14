67d5756e4392bf53a13278b70a7732ad6a745a9c
Pavel <naxa@nextmail.ru>
2023-10-14 15:10:14 +0200

    Get rid of property injection, use constructor injection [monolithic] (#4)

--- monolithic/monolithic-layered/src/main/java/com/epam/training/microservices/monolithic/service/symptom/SymptomService.java
+++ monolithic/monolithic-layered/src/main/java/com/epam/training/microservices/monolithic/service/symptom/SymptomService.java
@@ -1,18 +1,18 @@
 package com.epam.training.microservices.monolithic.service.symptom;
 
 import com.epam.training.microservices.monolithic.service.CrudService;
-
 import com.epam.training.microservices.monolithic.model.disease.Symptom;
 
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SymptomService implements CrudService<Symptom> {
-    @Autowired
-    private SymptomRepository symptomRepository;
+    private final SymptomRepository symptomRepository;
 
     @Override
     public JpaRepository<Symptom, Long> getRepository() {
